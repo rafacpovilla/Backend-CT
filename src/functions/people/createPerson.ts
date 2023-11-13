60a885456142c2c918ad1359a8e9656b7da9f3d1
Arthur Tosi <arthur.tosi@edu.ufes.br>
2023-11-13 17:30:53 -0300

    createPerson funcionando!

--- src/functions/people/createPerson.ts
+++ src/functions/people/createPerson.ts
@@ -1,18 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Handler } from "src/errors/Handler";
 import PeopleRepositories from "src/repositories/implementations/PeopleRepositories";
-//import ClientError from "src/errors/ClientError";
+import ClientError from "src/errors/ClientError";
 import { ok } from "src/utils/Returns";
 
 const createRoom = async (
     event: APIGatewayProxyEvent
   ): Promise<APIGatewayProxyResult> => {
   
-    const { name, email, empresa, password  } = JSON.parse(event.body);
+    const { name, email, empresa, senha  } = JSON.parse(event.body);
+    if (name === undefined || email === undefined || empresa === undefined || senha === undefined)
+        throw new ClientError("Algum campo não definido!");
 
     const database = new PeopleRepositories();
 
-    database.create(name, email, password, empresa);
+    database.create(name, email, senha, empresa);
         
     return ok("Pessoa criada com sucesso!", "message");
   };
