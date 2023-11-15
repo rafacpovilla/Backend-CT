43dbdcd5ab8abc89d62f1f9faa3a779eddd8f0b7
Rafaela Capovilla <rafacpovilla@gmail.com>
2023-11-14 22:49:20 -0300

    findById

---
+++ src/functions/rooms/findById.ts
@@ -0,0 +1,24 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { Handler } from "src/errors/Handler";
+import RoomsRepositories from "src/repositories/implementations/RoomsRepositories";
+import NotFoundError from "src/errors/NotFoundError";
+import { ok } from "src/utils/Returns";
+
+const findById = async (
+    event: APIGatewayProxyEvent
+  ): Promise<APIGatewayProxyResult> => {
+  
+    const { id } = JSON.parse(event.body);
+    if (id === undefined)
+        throw new NotFoundError("Id não encontrado!");
+
+    const database = new RoomsRepositories();
+
+    const room = await database.findById(id);
+    
+    console.log(room);
+    
+    return ok("message", room);
+  };
+  
+  export const handler = Handler(findById);
